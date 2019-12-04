d6f526abbbf05aa70b5047d10435f1f83d55ab44
Ian Harrigan <ianharrigan@hotmail.com>
2019-12-04 13:51:24 +0100

    erroneous extern?

--- src/libgit2/externs/LibGit2.hx
+++ src/libgit2/externs/LibGit2.hx
@@ -187,9 +187,9 @@ extern abstract CharStar(RawPointer<Char>) to(RawPointer<Char>) {
 	static public inline function fromString(s:String):CharStar
 		return new CharStar(s);
 
-	@:to extern public inline function toString():String
+	@:to public inline function toString():String
 		return new String(untyped this);
 
-	@:to extern public inline function toPointer():RawPointer<Char>
+	@:to public inline function toPointer():RawPointer<Char>
 		return this;
 }
